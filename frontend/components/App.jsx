bc337b457221330ebaf46fdffb9c097adc0470d2
jondominguez94 <jon.dominguez94@yahoo.com>
2018-12-21 08:33:30 -0800

    Reorganize imports

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+
+import { Switch, Redirect } from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+
 import NavbarContainer from './navbar/navbar_container';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import Splash from './session/splash';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
-import { AuthRoute, ProtectedRoute } from '../util/route_util';
+
 import BrowseContainer from './browse/browse_container';
-// import SearchContainer from './browse/search_results_container';
+// import SearchResultsContainer from './browse/search_results_container';
 import CurrentMovieContainer from './movies/current_movie_container';
-// import SearchPage from './browse/search_page.jsx';
-import Splash from './session/splash';
 
 const App = () => (
   <div className="overall">
@@ -16,14 +18,14 @@ const App = () => (
     <NavbarContainer />
 
     <Switch>
-      <ProtectedRoute path="/" component={NavbarContainer} />
       <AuthRoute exact path="/" component={Splash} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
   
       <ProtectedRoute path="/browse" component={BrowseContainer} />
-      {/* <ProtectedRoute path="/list" component={NavbarContainer} /> */}
+      {/* <ProtectedRoute path="/search" component={SearchResultsContainer} /> */}
       <ProtectedRoute path="/watch/:movieId" component={CurrentMovieContainer} />
+      {/* <ProtectedRoute path="/list" component={ListContainer} /> */}
       <Redirect to="/"/>
     </Switch>
   </div>
