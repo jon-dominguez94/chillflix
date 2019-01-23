7f6ea02c2b8a799166a4835ad9c183e29c023881
jondominguez94 <jon.dominguez94@yahoo.com>
2019-01-23 15:45:27 -0800

    Create a new list for every new user

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,6 +3,9 @@ class Api::UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       login(@user)
+      @list = List.new
+      @list.user_id = @user.id
+      @list.save
       render "api/users/show"
     else
       render json: @user.errors.full_messages, status: 422
--- app/views/api/users/_user.json.jbuilder
+++ app/views/api/users/_user.json.jbuilder
@@ -1,2 +1,3 @@
 json.extract! user, :id, :email, :profile_ids
+json.list_id user.list.id
 # json.imageUrl url_for(user.photo)
