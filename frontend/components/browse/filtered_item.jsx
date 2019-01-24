429dfd9a5d20a2eda90adec3cb55b990ff437cfd
jondominguez94 <jon.dominguez94@yahoo.com>
2019-01-24 10:01:42 -0800

    Style list button and convert filtered to class component

--- frontend/components/browse/filtered_item.jsx
+++ frontend/components/browse/filtered_item.jsx
@@ -1,41 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const FilteredItem = (props) => {
+class FilteredItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-  function renderList(){
-    if(props.onlist){
+  renderList(){
+    if(this.props.onlist){
       return(
-        <div>-</div>
+        <div className="circle-list-btn">-</div>
       );
     } else {
       return(
-        <div>+</div>
+        <div className="circle-list-btn">+</div>
       );
     }
   }
 
-  return(
+  render() {
+    return(
+      <div className="filtered-item">
+        {/* <img className="movie-tn" src={this.props.movie.thumbnail} /> */}
+        <img className="movie-tn" src={window.oldtn} />
 
-    <div className="filtered-item">
-      {/* <img className="movie-tn" src={props.movie.thumbnail} /> */}
-      <img className="movie-tn" src={window.oldtn} />
 
+        <div className="search-overlay">
 
-      <div className="search-overlay">
+          <div className="search-play">
+            <Link to={`/watch/${this.props.movie.id}`}>
+              <i className="fa fa-play-circle-o"></i>
+            </Link>
+          </div>
 
-        <div className="search-play">
-          <Link to={`/watch/${props.movie.id}`}>
-            <i className="fa fa-play-circle-o"></i>
-          </Link>
         </div>
 
+        <p className="filter-title">{this.props.movie.title}</p>
+        <div className="circle-list">
+          {this.renderList()}
+        </div>
       </div>
-
-      <p className="filter-title">{props.movie.title}</p>
-      {renderList()}
-    </div>
-  );
+    );
+  }
 };
 
 export default FilteredItem;
