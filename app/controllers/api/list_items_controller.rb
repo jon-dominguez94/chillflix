d3cec980f3b58244177f8ce2da3bf6116a50cfd6
jondominguez94 <jon.dominguez94@yahoo.com>
2019-01-23 18:27:02 -0800

    Write list items controller

--- app/controllers/api/list_items_controller.rb
+++ app/controllers/api/list_items_controller.rb
@@ -1,10 +1,28 @@
 class Api::ListItemsController < ApplicationController
+
+  before_action :require_login
+
   def create
+    @list_item = ListItem.new(list_item_params)
+    @list_item.list_id = current_user.list_id
+    if @list_item.save
+      render "api/list_items/show"
+    else
+      render json: @list_item.errors.full_messages, status: 422
+    end
   end
 
-  def delete
+  def destroy
+    @list_item = ListItem.find(params[:id])
+    @list_item.destroy
   end
 
   def show
+    @list_item = ListItem.find(params[:id])
+    render "api/list_items/show"
+  end
+
+  def list_item_params
+    params.require(:list_item).permit(:movie_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
 
-  namespace :api do
-    get 'list_items/create'
-    get 'list_items/delete'
-    get 'list_items/show'
-  end
   namespace :api, defaults: {format: :json} do
     resource :session, only: [:create, :destroy]
     resource :user, only: [:create]
     resources :profiles, only: [:index, :create, :update, :destroy]
     resources :movies, only: [:index, :show]
     resources :lists, only: [:show]
+    resources :list_items, only: [:create, :destroy, :show]
   end
   root to: 'static_pages#root'
 end
