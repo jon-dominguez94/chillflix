1ecead214b78404270889b68c8c85965071c1f8e
jondominguez94 <jon.dominguez94@yahoo.com>
2019-01-29 18:00:14 -0800

    Add all footer links

--- frontend/components/navbar/footer.jsx
+++ frontend/components/navbar/footer.jsx
@@ -2,7 +2,30 @@ import React from 'react';
 
 const Footer = props => {
   return (
-    <div>Footer</div>
+    <div>
+      <p className="questions">Questions? Call 1-866-579-3241</p>
+      <div className="footer-links">
+        <ul className="links-list">
+          <li>FAQ</li>
+          <li>Help Center</li>
+          <li>Account</li>
+          <li>Media Center</li>
+          <li>Investor Relations</li>
+          <li>Jobs</li>
+          <li>Redeem Gift Cards</li>
+          <li>Buy Gift Cards</li>
+          <li>Ways to Watch</li>
+          <li>Terms of Use</li>
+          <li>Privacy</li>
+          <li>Cookie References</li>
+          <li>Corporate Information</li>
+          <li>Contact Us</li>
+          <li>Speed Test</li>
+          <li>Legal Notices</li>
+          <li>Chillflix Originals</li>
+         </ul>
+      </div>
+    </div>
   );
 };
 
