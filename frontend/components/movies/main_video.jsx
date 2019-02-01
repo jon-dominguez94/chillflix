34319e490e62df4221e652637c9d0d2f74ca7070
jondominguez94 <jon.dominguez94@yahoo.com>
2019-02-01 09:25:33 -0800

    Add proper gif to main video

--- frontend/components/movies/main_video.jsx
+++ frontend/components/movies/main_video.jsx
@@ -42,18 +42,16 @@ class MainVideo extends React.Component {
   render() {
     return (
       <div className="main-thumb">
-        {/* <div className="main-gif"></div> */}
-        <img src={window.oldtn}/>
-        
+        <div className="main-gif"></div>
+        {/* <img src={window.oldtn}/> */}
 
         <div className="main-video-info">
-
           <h1 className="main-video-title">{this.props.video.title}</h1>
           <div className="main-video-links">
             <Link to={`/watch/${this.props.video.id}`}>
               <div className="play-btn">
                 <div className="main-video-link">
-                  <button className='button play'></button>
+                  <button className="button play" />
                   <span>Play</span>
                 </div>
               </div>
@@ -64,12 +62,14 @@ class MainVideo extends React.Component {
                 <div className="plus-wrapper">
                   {/* <i className="fa fa-plus"></i> */}
                   {this.renderButton()}
-                  </div>
+                </div>
                 <span>My List</span>
-              </div>  
+              </div>
             </div>
           </div>
-          <h2 className="main-video-description">{this.props.video.description}</h2>
+          <h2 className="main-video-description">
+            {this.props.video.description}
+          </h2>
         </div>
       </div>
     );
